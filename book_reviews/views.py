0682563e759aba28370304ac78c4dc4066d6e460
Nico Foth <nico.foth@online.de>
2020-11-18 17:51:56 +0100

    Added base template aswell as the first view of the book_reviews application

--- book_reviews/views.py
+++ book_reviews/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def show_articles(request):
-    return HttpResponse("HalloTest123")
+    return render(request, "book_reviews/show_articles.html")

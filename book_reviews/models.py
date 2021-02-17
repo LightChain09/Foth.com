ef96df50f83b75983d9b3c7eb602c3dafdf11dad
Nico Foth <nico.foth@online.de>
2021-02-17 18:46:07 +0100

    Removed book tags from book_review model

--- book_reviews/models.py
+++ book_reviews/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-TAG_CHOICES = (
-    ("misc", "miscellaneous"),
-    ("", ""),
-    ("", ""),
-    ("", ""),
-)
 
 class BookReviewArticle(models.Model):
     book_title = models.CharField(max_length=64)
-    book_tags = models.CharField(max_length=32, choices=TAG_CHOICES, default="misc")
     introduction = models.CharField(max_length=2000, default="*Introduction*")
     review_content = models.CharField(max_length=25000)
     article_date = models.DateField(auto_now=True)

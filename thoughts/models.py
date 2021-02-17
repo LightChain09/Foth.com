111de7bd11853c76d41e04098127b635b7601585
Nico Foth <nico.foth@online.de>
2021-02-17 01:59:32 +0100

    Added model for thoughts app, registered it in admin, added it in views

--- thoughts/models.py
+++ thoughts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class ThoughtArticles(models.Model):
+    title = models.CharField(max_length=200)
+    publish_date = models.DateField(auto_now=True)
+    article_content = models.CharField(max_length=25000)

467a9ba79acbf65f339e979cbfcc7673e54b46b5
Nico Foth <nico.foth@online.de>
2021-02-17 01:59:32 +0100

    Added model for thoughts app, registered it in admin, added it in views

--- thoughts/views.py
+++ thoughts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import ThoughtArticles
 
 def show_thought_posts(request):
-    return render(request, "thoughts/show_thought_posts.html")
+    articles = ThoughtArticles.objects.all()
+    return render(request, "thoughts/show_thought_posts.html", {"articles": articles})

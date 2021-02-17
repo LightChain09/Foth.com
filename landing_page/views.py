95bf6919de002391f020088f3c974b35474e9f48
Nico Foth <nico.foth@online.de>
2021-02-17 18:43:30 +0100

    Added exact age calculation to landing page

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+import datetime
+
+def calculate_age(birth):
+    now = datetime.datetime.now()
+    difference = now - birth
+
+    days = difference.days
+
+    years = days / 365
+    
+    return years
+
 
 def show_landing_page(request):
-    return render(request, 'landing_page/index.html')
+
+    age = calculate_age(datetime.datetime(year=2002, month=9, day=3, hour=14))
+
+    
+    
+
+
+    return render(request, 'landing_page/index.html', {"age": age})

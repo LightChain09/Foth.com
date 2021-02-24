b57360fabfec09a8f1bcede22e4a557f0057f112
root <root@foth.dev-webservice>
2021-02-24 20:27:41 +0000

    fixed csgo/urls.py

--- csgo/urls.py
+++ csgo/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.show_csgo_stats, name="csgo_stats_show"),
+    path('', views.show_csgo_stats, name="show_csgo_stats"),
 ]

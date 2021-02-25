dfcffdf1c5743697fa9b22a49cba2c61f06206a1
LightChain09 <nico.foth@online.de>
2021-02-25 17:16:20 +0100

    Added an automatic team generator for the csgo app

--- csgo/views.py
+++ csgo/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from .models import CsgoPlayer
+from .forms import SelectPlayers
+from .generate_team import generate
 
 def show_csgo_stats(request):
     
     player_data = CsgoPlayer.objects.order_by("-elo")
 
-    return render(request, "csgo/csgo_stats_show.html", {"player_data": player_data})
+    return render(request, "csgo/show_csgo_stats.html", {"player_data": player_data})
+
+def select_players(request):
+
+    if request.method == "POST":
+
+        form = SelectPlayers(request.POST)
+        if form.is_valid():
+            selected_players = form.clean()
+
+            player_dict = {}
+            queryset = None
+
+            for dict in selected_players:
+                queryset = selected_players[dict]
+
+            for item in queryset:
+                player_dict[item.name] = item.elo
+            
+            team_a, team_b = generate(player_dict)
+        
+        return render(request, "csgo/show_teams.html", {"team_a": team_a, "team_b": team_b})
+
+    else:
+
+        selectform = SelectPlayers()
+        
+        return render(request, "csgo/select_players.html", {"selectform": selectform})

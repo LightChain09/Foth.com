4f8e632848737c95fae5672f0f0b73e5d7dffedb
LightChain09 <nico.foth@online.de>
2021-02-27 14:22:56 +0100

    Added odd number csgo team generations

--- csgo/generate_team.py
+++ csgo/generate_team.py
@@ -7,17 +7,17 @@ def generate(players):
     team_b = []
     player_names_sorted = list(sorted_dict.keys())
 
+    team_a.append(player_names_sorted[0])
+    team_a.append(player_names_sorted[-1])
+    team_b.append(player_names_sorted[1])
+    team_b.append(player_names_sorted[-2])
+    
+    for _ in range(2):
+        player_names_sorted.pop(0)
+        player_names_sorted.pop(-1)
+
     if len(sorted_dict) % 2 == 0:
-        team_a.append(player_names_sorted[0])
-        team_a.append(player_names_sorted[-1])
-        team_b.append(player_names_sorted[1])
-        team_b.append(player_names_sorted[-2])
-
-        player_names_sorted.pop(0)
-        player_names_sorted.pop(0)
-        player_names_sorted.pop(-1)
-        player_names_sorted.pop(-1)
-
+    
         for _ in range((len(player_names_sorted)//2)):
             current_elo = [0, 0]
             for name in team_a:
@@ -30,10 +30,29 @@ def generate(players):
                 team_b.append(player_names_sorted[1])
                 player_names_sorted.pop(0)
                 player_names_sorted.pop(0)
+       
             else:
                 team_b.append(player_names_sorted[0])
                 team_a.append(player_names_sorted[1])
                 player_names_sorted.pop(0)
                 player_names_sorted.pop(0)
+
+    else:
+
+        for _ in range(len(player_names_sorted)):
+            current_elo = [0, 0]
+
+            for name in team_a:
+                current_elo[0] += sorted_dict[name]
+            for name in team_b:
+                current_elo[1] += sorted_dict[name]
+
+            if current_elo[0] < current_elo[1]:
+                team_a.append(player_names_sorted[0])
+            else:
+                team_b.append(player_names_sorted[0])
+            
+            player_names_sorted.pop(0)
+
 
     return team_a, team_b

ec8d6d8f4d5d087287daed83e7d22efeafe03ef4
LightChain09 <nico.foth@online.de>
2021-03-04 18:27:20 +0100

    Added a view function for the csgo player profile

--- csgo/urls.py
+++ csgo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
     path('', views.show_csgo_stats, name="show_csgo_stats"),
-    path('select-players', views.select_players, name="select_players")
+    path('select-players', views.select_players, name="select_players"),
+    path('<str:steam_id>', views.player_profile, name="player_profile")
 ]

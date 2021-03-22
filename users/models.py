8ac06bdd6552c7a0609adfc7bb8ca876b5579c38
LightChain09 <nico.foth@online.de>
2021-03-22 15:12:44 +0100

    Added user profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import django.contrib.auth.models as auth_models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(auth_models.User, on_delete=models.CASCADE)
+    steam_id = models.CharField(max_length=18, default="")
+
+
+    def __str__(self):
+        return f"{self.user.username} Profile"

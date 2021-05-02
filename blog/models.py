f62b2f1c624aa7c8929d4639d45fc284d3418fc5
Nico Foth <nico.foth@online.de>
2021-05-02 23:31:16 +0200

    Redesigned blog page and added introduction field in blog model

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class BlogArticle(models.Model):
     title = models.CharField(max_length=64)
+    introduction = models.TextField()
     publish_date = models.DateTimeField(auto_now=True)
     content = models.TextField()

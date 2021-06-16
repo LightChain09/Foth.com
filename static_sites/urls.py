0733b623523361e685e46029ebcb25b751d3dc0f
Nico Foth <nico.foth@online.de>
2021-06-16 23:20:25 +0200

    Added css timelines to the about page

--- static_sites/urls.py
+++ static_sites/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
     path('', views.show_landing_page, name="show_landing_page"),
+    path('about/', views.show_about_page, name="show_about_page"),
     path('legal/', views.show_legal_page, name="show_legal_page")
 ]

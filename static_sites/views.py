e5c662673c5302b95146aa27594782416e1bbbc9
Nico Foth <nico.foth@online.de>
2021-06-16 23:20:25 +0200

    Added css timelines to the about page

--- static_sites/views.py
+++ static_sites/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 import datetime
 
-def show_legal_page(request):
-    return render(request, 'static_sites/legal.html')
-
-
 def calculate_age(birth):
     now = datetime.datetime.now()
     difference = now - birth
@@ -16,8 +12,15 @@ def calculate_age(birth):
     return years
 
 
-def show_landing_page(request):
+def show_legal_page(request):
+    return render(request, 'static_sites/legal.html')
 
+
+def show_about_page(request):
+    return render(request, 'static_sites/about.html')
+
+
+def show_landing_page(request):
     age = calculate_age(datetime.datetime(year=2002, month=9, day=3, hour=14))
 
     user = request.user

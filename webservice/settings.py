b4c40bb86304ca88e5a04f6e68e605baec6b72ff
Nico Foth <nico.foth@online.de>
2021-07-06 02:00:30 +0200

    Added text to the about page and refined the design

--- webservice/settings.py
+++ webservice/settings.py
@@ -23,7 +23,7 @@ BASE_TEMPLATE = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = '+z5at85^&w-uhp7z!6u!+^9+a6a(pm5=r^riudju&$$32t6l5_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["207.154.246.228", "foth.dev", "www.foth.dev", "localhost"]
 
